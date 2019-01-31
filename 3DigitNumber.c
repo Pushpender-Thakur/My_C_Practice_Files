4afd3ae81c259bc90f9a6cbb85d4dd40350c0121
Pushpender Thakur <46690325+Pushpender-Thakur@users.noreply.github.com>
2019-01-31 20:51:54 +0530

    Add files via upload

---
+++ 3DigitNumber.c
@@ -0,0 +1,529 @@
+#include <stdio.h>
+int main (void)
+{
+    int UserNumber,Hundred,Tens,Ones;
+    printf("Enter Number: ");
+    scanf("%d",&UserNumber);
+    if (UserNumber >= 100 && UserNumber <= 999)
+    {
+        if (UserNumber >= 100 && UserNumber < 200)
+        {
+            printf("1\n");
+            Hundred = UserNumber - 100;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else if (UserNumber >= 200 && UserNumber < 300)
+        {
+            printf("2\n");
+            Hundred = UserNumber - 200;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else if (UserNumber >= 300 && UserNumber < 400)
+        {
+            printf("3\n");
+            Hundred = UserNumber - 300;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else if (UserNumber >= 400 && UserNumber < 500)
+        {
+            printf("4\n");
+            Hundred = UserNumber - 400;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else if (UserNumber >= 500 && UserNumber < 600)
+        {
+            printf("5\n");
+            Hundred = UserNumber - 500;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else if (UserNumber >= 600 && UserNumber < 700)
+        {
+            printf("6\n");
+            Hundred = UserNumber - 600;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else if (UserNumber >= 700 && UserNumber < 800)
+        {
+            printf("7\n");
+            Hundred = UserNumber - 700;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else if (UserNumber >= 800 && UserNumber < 900)
+        {
+            printf("8\n");
+            Hundred = UserNumber - 800;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        } else
+        {
+            printf("9\n");
+            Hundred = UserNumber - 900;
+            Tens = Hundred / 10;
+            if (Tens == 0)
+            {
+                printf("0\n");
+                printf("%d",Hundred);
+            } else if (Tens == 1)
+            {
+                printf("1\n");
+                Ones = Hundred - 10;
+                printf("%d",Ones);
+            } else if (Tens == 2)
+            {
+                printf("2\n");
+                Ones = Hundred - 20;
+                printf("%d",Ones);
+            } else if (Tens == 3)
+            {
+                printf("3\n");
+                Ones = Hundred - 30;
+                printf("%d",Ones);
+            } else if (Tens == 4)
+            {
+                printf("4\n");
+                Ones = Hundred - 40;
+                printf("%d",Ones);
+            } else if (Tens == 5)
+            {
+                printf("5\n");
+                Ones = Hundred - 50;
+                printf("%d",Ones);
+            } else if (Tens == 6)
+            {
+                printf("6\n");
+                Ones = Hundred - 60;
+                printf("%d",Ones);
+            } else if (Tens == 7)
+            {
+                printf("7\n");
+                Ones = Hundred - 70;
+                printf("%d",Ones);
+            } else if (Tens == 8)
+            {
+                printf("8\n");
+                Ones = Hundred - 80;
+                printf("%d",Ones);
+            } else
+            {
+                printf("9\n");
+                Ones = Hundred - 90;
+                printf("%d",Ones);
+            }
+            
+            
+        }
+    } else
+    {
+        printf("This Program Only Works With 3 digit Positive Integer!!");
+    }
+    
+    return 0;
+}
