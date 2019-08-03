0f4e7fe727bc22f2869aa9507f4a5a558b442042
Pushpender Thakur <46690325+Pushpender-Thakur@users.noreply.github.com>
2019-08-03 18:34:33 +0530

    Add files via upload

--- mergetwoarrays.c
+++ mergetwoarrays.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-    unsigned int sizeOfArr1, sizeOfArr2, i;
+    unsigned int sizeOfArr1, sizeOfArr2;
+    int i;
 
     // Array 1
     printf("Enter First Array Size: ");
@@ -23,52 +24,211 @@ int main(void)
 
     int arr3[sizeOfArr1 + sizeOfArr2];
     unsigned int index = 0;
-    unsigned int j;
+    int j;
 
-    for (i = 0, j = 0; i < sizeOfArr1 && j < sizeOfArr2; i++, j++)
-    {
-        if (arr1[i] > arr2[j])
-        {
-            arr3[index] = arr2[j];
-            index++;
-            i--;
-        }
-        else if (arr1[i] < arr2[j])
-        {
-            arr3[index] = arr1[i];
-            index++;
-            j--;
-        }
-        else
-        {
-            arr3[index] = arr1[i];
-            index++;
-        }
-    }
+    unsigned int A_Arr1_Flag = arr1[0] > arr1[sizeOfArr1 - 1] ? 0 : 1;
+    unsigned int A_Arr2_Flag = arr2[0] > arr2[sizeOfArr2 - 1] ? 0 : 1;
 
-    if (j == sizeOfArr2 && i < sizeOfArr1)
+
+    // Ascending - Ascending
+    if (A_Arr1_Flag == 1 && A_Arr2_Flag == 1)
     {
-        for (i; i < sizeOfArr1; i++)
+        for (i = 0, j = 0; i < sizeOfArr1 && j < sizeOfArr2; i++, j++)
         {
-            if (arr1[i] != arr3[index - 1])
+            if (arr1[i] > arr2[j])
+            {
+                arr3[index] = arr2[j];
+                index++;
+                i--;
+            }
+            else if (arr1[i] < arr2[j])
+            {
+                arr3[index] = arr1[i];
+                index++;
+                j--;
+            }
+            else
             {
                 arr3[index] = arr1[i];
                 index++;
             }
         }
-    }
-    else if (i == sizeOfArr1 && j < sizeOfArr2)
-    {
-        for (j; j < sizeOfArr2; j++)
+
+        if (j == sizeOfArr2 && i < sizeOfArr1)
         {
-            if (arr2[j] != arr3[index - 1])
+            for (i; i < sizeOfArr1; i++)
             {
-                arr3[index] = arr2[j];
-                index++;
+                if (arr1[i] != arr3[index - 1])
+                {
+                    arr3[index] = arr1[i];
+                    index++;
+                }
+            }
+        }
+        else if (i == sizeOfArr1 && j < sizeOfArr2)
+        {
+            for (j; j < sizeOfArr2; j++)
+            {
+                if (arr2[j] != arr3[index - 1])
+                {
+                    arr3[index] = arr2[j];
+                    index++;
+                }
             }
         }
     }
 
+    // Decending  - Decending
+    else if (A_Arr1_Flag == 0 && A_Arr2_Flag == 0)
+    {
+        for (i = sizeOfArr1 - 1, j = sizeOfArr2 - 1; i >= 0 && j >= 0; i--, j--)
+        {
+            if (arr1[i] > arr2[j])
+            {
+                arr3[index] = arr2[j];
+                index++;
+                i++;
+            }
+            else if (arr1[i] < arr2[j])
+            {
+                arr3[index] = arr1[i];
+                index++;
+                j++;
+            }
+            else
+            {
+                arr3[index] = arr1[i];
+                index++;
+            }
+            
+        }
+
+        if (j > 0 && i == -1)
+        {
+            for (j; j >= 0; j--)
+            {
+                if (arr3[index - 1] != arr2[j])
+                {
+                    arr3[index] = arr2[j];
+                    index++;
+                }
+            }
+        }
+        else if (i > 0 && j == -1)
+        {
+            for (i; i >= 0; i--)
+            {
+                if (arr3[index - 1] != arr1[i])
+                {
+                    arr3[index] = arr1[i];
+                    index++;
+                }
+            }
+        }
+        
+    }
+
+    // Ascending - Decending
+    else if (A_Arr1_Flag == 1 && A_Arr2_Flag == 0)
+    {
+        for (i = 0, j = sizeOfArr2 - 1; i < sizeOfArr1 && j >= 0; i++,j--)
+        {
+            if (arr1[i] > arr2[j])
+            {
+                arr3[index] = arr2[j];
+                index++;
+                i--;
+            }
+            else if (arr1[i] < arr2[j])
+            {
+                arr3[index] = arr1[i];
+                index++;
+                j++;
+            }
+            else
+            {
+                arr3[index] = arr1[i];
+                index++;
+            }
+            
+        }
+
+        if (i == sizeOfArr1)
+        {
+            for (j; j >= 0; j--)
+            {
+                if (arr3[index - 1] != arr2[j])
+                {
+                    arr3[index] = arr2[j];
+                    index++;
+                }
+            }
+        }
+        else if (j == -1)
+        {
+            for (i; i < sizeOfArr1; i++)
+            {
+                if (arr3[index - 1] != arr1[i])
+                {
+                    arr3[index] = arr1[i];
+                    index++;
+                }
+            }
+        }
+    }
+
+    // Decending - Ascending
+    else
+    {
+        for (i = sizeOfArr1 - 1,j = 0; i >= 0, j < sizeOfArr2; i--,j++)
+        {
+            if (arr1[i] > arr2[j])
+            {
+                arr3[index] = arr2[j];
+                index++;
+                i++;
+            }
+            else if (arr1[i] < arr2[j])
+            {
+                arr3[index] = arr1[i];
+                index++;
+                j--;
+            }
+            else
+            {
+                arr3[index] = arr1[i];
+                index++;
+            }
+            
+        }
+
+        if (i == -1)
+        {
+            for (j; j < sizeOfArr2; j++)
+            {
+                if (arr3[index - 1] != arr2[j])
+                {
+                    arr3[index] = arr2[j];
+                    index++;
+                }
+            }
+        }
+        else if (j == sizeOfArr2)
+        {
+            for (i; i >= 0; i--)
+            {
+                if (arr3[index - 1] != arr1[i])
+                {
+                    arr3[index] = arr1[i];
+                    index++;
+                }
+            }
+        }
+        
+    }
+    
+
+    printf("\nArray 3: - \n");
     for (i = 0; i < index; i++)
     {
         printf("%d ", arr3[i]);
